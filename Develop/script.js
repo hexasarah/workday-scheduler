2ad7f8c8ec3060ee6e5d96bf64a698cd59da1922
sarah chambers <chamberssarahann@gmail.com>
2024-03-14 22:08:16 -0700

    removed comments, cleaned up code

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
   const saveButtons = document.querySelectorAll('.saveBtn');
   saveButtons.forEach(saveButton => {
     saveButton.addEventListener('click', function() {
@@ -16,12 +8,7 @@ $(function () {
       saveToLocalStorage(time);
     });
   });
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  
   const currentHour = dayjs().hour();
   const timeBlocks = document.querySelectorAll('.time-block');
   timeBlocks.forEach(timeBlock => {
@@ -35,11 +22,7 @@ $(function () {
       timeBlock.classList.add('future');
     }
   });
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
   const loadFromLocalStorage = function() {
     const timeBlocks = document.querySelectorAll('.time-block');
     timeBlocks.forEach(timeBlock => {
@@ -59,7 +42,6 @@ $(function () {
     const inputValue = textArea.value;
     localStorage.setItem(time, inputValue);
   }
-  // TODO: Add code to display the current date in the header of the page.
   const currentDay = dayjs().format('MMM-DD')
   const dayElement = document.getElementById('currentDate')
   dayElement.textContent = currentDay;
